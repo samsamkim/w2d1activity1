df9af74a4be6da4bde641538360dcb752b320912
samsamkim <hoonshik10@gmail.com>
2018-04-16 19:44:07 +0000

    third step done

---
+++ step3.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+
+
+function getAndPrintHTML (options) {
+
+
+
+
+  https.get(options, function(response){
+  response.setEncoding('utf8');
+
+    var addedChunks = "";
+    response.on('data', function (data){
+      addedChunks += data;
+      console.log(addedChunks);
+    })
+
+  })
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+
+getAndPrintHTML(requestOptions);
